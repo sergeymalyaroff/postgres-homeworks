3dd144ef67884be3d204d782fc33778752966e68
Sergey Malyarov <sergey@malyaroff.com>
2023-07-26 17:15:20 +0300

    HW-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees (
+  employee_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  title VARCHAR(50),
+  birth_date DATE,
+  notes TEXT
+);
+
+CREATE TABLE customers (
+  customer_id VARCHAR(50) PRIMARY KEY,
+  company_name VARCHAR(100),
+  contact_name VARCHAR(100)
+);
+
+CREATE TABLE orders (
+  order_id SERIAL PRIMARY KEY,
+  customer_id VARCHAR(50),
+  employee_id INT,
+  order_date DATE,
+  ship_city VARCHAR(100),
+  FOREIGN KEY (customer_id) REFERENCES customers (customer_id),
+  FOREIGN KEY (employee_id) REFERENCES employees (employee_id)
+);
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,54 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+
+# создаем соединение
+conn = psycopg2.connect(
+    dbname='north',
+    user='postgres',
+    password='renuar',
+    host='localhost'
+)
+
+# создаем курсор
+cur = conn.cursor()
+
+# открываем CSV-файл с данными
+with open('north_data/employees_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)  # пропускаем заголовок
+    for row in reader:
+        # вставляем данные в таблицу
+        cur.execute(
+            "INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+            row
+        )
+
+# для таблицы customers
+with open('north_data/customers_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)  # пропускаем заголовок
+    for row in reader:
+        cur.execute(
+            "INSERT INTO customers VALUES (%s, %s, %s)",
+            row
+        )
+
+# для таблицы orders
+with open('north_data/orders_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)  # пропускаем заголовок
+    for row in reader:
+        cur.execute(
+            "INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+            row
+        )
+
+
+# сохраняем изменения
+conn.commit()
+
+# закрываем соединение
+cur.close()
+conn.close()
